7947e4df66bdaaa96fc9678512af2601d7b32fb7
Jjosephi <31075794+FlavorlessQuark@users.noreply.github.com>
2024-05-21 15:27:40 -0700

    Added xdg portal for electron

--- hyprland.sh
+++ hyprland.sh
@@ -1,5 +1,5 @@
-sudo pacman -Sy hyprland kitty alacritty xdg-desktop-portal-hyprland
-
+sudo pacman -Sy hyprland kitty alacritty xdg-desktop-portal-hyprland xdg-desktop-portal-gtk
+# xdg-portal-gtk for file-chooser since hyprland doesnt have one (needed for electron)
 sudo cp -r hypr ~/.config/hypr
 sudo cp -r waybar ~/.config/waybar
 sudo cp -r neofetch ~/.config/neofetch
